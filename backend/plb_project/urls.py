aafe7f3c1f3659aac791c246517c176e07d3c0e8
Glauber Roberto Schmit <glauberschmit@gmail.com>
2025-10-15 00:08:44 -0300

    Solucao Final 2: Mapeamento de URL direto no plb_project.urls para corrigir 404 persistente

--- backend/plb_project/urls.py
+++
@@ -1,8 +0,0 @@
-# backend/plb_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/licitacoes/', include('licitacoes.urls')),
-]
